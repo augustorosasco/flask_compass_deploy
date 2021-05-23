b888222bc891cf266df15b3cbec4a640c8b517e1
AUGUSTO ROSASCO <52939868+augustorosasco@users.noreply.github.com>
2021-05-22 23:40:04 -0300

    initial commit

--- flask_compass/__init__.py
+++ flask_compass/__init__.py
@@ -8,7 +8,6 @@ from flask_compass.routes.main import main
 from flask_compass.routes.LoginController import LoginController
 from flask_compass.routes.AcessoController import AcessoController
 from flask_compass.routes.ItemController import ItemController
-from flask_compass.routes.AvaliaController import AvaliaController
 
 def create_app(config_file='settings.py'):
     app = Flask(__name__)
@@ -29,7 +28,6 @@ def create_app(config_file='settings.py'):
     app.register_blueprint(LoginController)
     app.register_blueprint(AcessoController)
     app.register_blueprint(ItemController)
-    app.register_blueprint(AvaliaController)
     app.register_blueprint(main)
 
     app.cli.add_command(create_tables)
